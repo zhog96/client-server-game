6c23e15baf7863f44a96f47f4bc7baab5dc0e4e1
zhogo <zhogov.aa@phystech.edu>
2024-12-18 00:40:50 +0300

    Server side by frame logic

--- multiplayer/src/commonMain/kotlin/com/game/multiplayer/client/ServerApi.kt
+++ multiplayer/src/commonMain/kotlin/com/game/multiplayer/client/ServerApi.kt
@@ -1,8 +1,10 @@
 package com.game.multiplayer.client
 
+import com.game.multiplayer.connect.Action
 import com.game.multiplayer.connect.EntitiesState
 
 interface ServerApi {
     fun serverState(): EntitiesState
     fun offerState(state: EntitiesState)
+    fun pushAction(action: Action)
 }
